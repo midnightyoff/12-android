8097eea47c355b8287bbc36e25ebeb6f775f46fb
midnightyoff <89267375+midnightyoff@users.noreply.github.com>
2026-04-13 19:35:33 +0700

    Add event card (#1)
    * Add event card
    
    * Refactor event type text

---
+++ app/src/main/java/com/eltex/androidschool/EventCard.kt
@@ -0,0 +1,198 @@
+package com.eltex.androidschool
+
+import android.content.res.Configuration
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.eltex.androidschool.ui.theme.AndroidTheme
+
+@Composable
+fun EventCard(
+    event: EventUiModel,
+    modifier: Modifier = Modifier,
+    menuClicked: () -> Unit = {},
+    likeClicked: () -> Unit = {},
+    shareClicked: () -> Unit = {},
+    participateClicked: () -> Unit = {}
+) {
+    Card(
+        modifier.fillMaxWidth(),
+        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.background)
+    ) {
+        Column(modifier = Modifier.padding(top = 4.dp, bottom = 4.dp, start = 16.dp)) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 12.dp, bottom = 12.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Box(
+                    modifier = Modifier
+                        .size(40.dp)
+                        .background(MaterialTheme.colorScheme.primary, shape = CircleShape),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(text = event.author.take(1), color = MaterialTheme.colorScheme.onPrimary)
+                }
+
+                Spacer(Modifier.width(16.dp))
+
+                Column(modifier = Modifier.weight(1F)) {
+                    Text(
+                        text = event.author,
+                        fontWeight = FontWeight.W500,
+                        fontSize = 16.sp
+                    )
+                    Text(
+                        text = event.published,
+                        fontWeight = FontWeight.W400,
+                        fontSize = 14.sp
+                    )
+                }
+
+                IconButton(menuClicked, modifier = Modifier.padding(end = 4.dp)) {
+                    Icon(Icons.Default.MoreVert, "More")
+                }
+            }
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 8.dp, end = 16.dp)
+            ) {
+                Text(
+                    text = if (event.type == EventType.OFFLINE) {
+                        stringResource(R.string.offline_type)
+                    } else stringResource(R.string.online_type),
+                    fontWeight = FontWeight.W400,
+                    fontSize = 16.sp
+                )
+                Text(
+                    text = event.datetime,
+                    fontSize = 14.sp
+                )
+            }
+
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 32.dp, end = 16.dp),
+                text = event.content,
+                fontSize = 14.sp
+            )
+
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 32.dp, end = 16.dp),
+                text = event.link,
+                fontSize = 14.sp
+            )
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 32.dp, end = 16.dp)
+            ) {
+                TextButton(likeClicked) {
+                    Icon(
+                        if (event.likedByMe) Icons.Default.Favorite
+                        else Icons.Default.FavoriteBorder,
+                        "Like",
+                        tint = MaterialTheme.colorScheme.primary
+                    )
+                    Spacer(Modifier.width(8.dp))
+                    Text(event.likes.toString(), fontWeight = FontWeight.W500)
+                }
+                IconButton(shareClicked, modifier = Modifier.padding(end = 8.dp)) {
+                    Icon(
+                        Icons.Default.Share,
+                        "Share",
+                        tint = MaterialTheme.colorScheme.primary
+                    )
+                }
+
+                Spacer(modifier = Modifier.weight(1f))
+
+                TextButton(participateClicked) {
+                    Icon(
+                        if (event.participatedByMe) painterResource(R.drawable.ic_participate)
+                        else painterResource(R.drawable.ic_participate_outlined),
+                        "Participate",
+                        tint = MaterialTheme.colorScheme.primary
+                    )
+                    Spacer(Modifier.width(8.dp))
+                    Text(event.participants.toString(), fontWeight = FontWeight.W500)
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun EventCardPreview() {
+    AndroidTheme {
+        EventCard(
+            EventUiModel(
+                id = 1L,
+                author = "Lydia Westervelt",
+                published = "11.05.22 11:21",
+                type = EventType.OFFLINE,
+                datetime = "16.05.22 12:00",
+                content = "Приглашаю провести уютный вечер за увлекательными играми! У нас есть несколько вариантов настолок, подходящих для любой компании.",
+                link = "https://m2.material.io/components/cards",
+                likes = 2,
+                participants = 2
+            )
+        )
+    }
+}
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES or Configuration.UI_MODE_TYPE_NORMAL)
+@Composable
+fun EventCardPreviewDark() {
+    AndroidTheme {
+        EventCard(
+            EventUiModel(
+                id = 1L,
+                author = "Lydia Westervelt",
+                published = "11.05.22 11:21",
+                type = EventType.OFFLINE,
+                datetime = "16.05.22 12:00",
+                content = "Приглашаю провести уютный вечер за увлекательными играми! У нас есть несколько вариантов настолок, подходящих для любой компании.",
+                link = "https://m2.material.io/components/cards",
+                likes = 2,
+                participants = 2
+            )
+        )
+    }
+}
---
+++ app/src/main/java/com/eltex/androidschool/EventUiModel.kt
@@ -0,0 +1,19 @@
+package com.eltex.androidschool
+
+data class EventUiModel(
+    val id: Long = 0,
+    val author: String = "",
+    val published: String = "",
+    val type: EventType = EventType.OFFLINE,
+    val datetime: String = "",
+    val content: String = "",
+    val link: String = "",
+    val likedByMe: Boolean = false,
+    val likes: Int = 0,
+    val participatedByMe: Boolean = false,
+    val participants: Int = 0,
+)
+
+enum class EventType {
+    OFFLINE, ONLINE
+}
