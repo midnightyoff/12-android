a6a80d80075f42c8815cc41e88d0a3fc38456c9b
midnightyoff <89267375+midnightyoff@users.noreply.github.com>
2026-04-24 23:19:41 +0700

    Migrate to LazyColumn, add date separators and animations for like\participate buttons (#5)

--- app/src/main/java/com/eltex/androidschool/feauture/event/EventViewModel.kt
+++ app/src/main/java/com/eltex/androidschool/feauture/event/EventViewModel.kt
@@ -8,46 +8,72 @@ import androidx.lifecycle.ViewModel
 import com.eltex.androidschool.R
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
+import java.time.Instant
+import java.time.ZoneId
 
-class EventViewModel : ViewModel() {
-    var state by mutableStateOf(createEvent())
+class EventListViewModel : ViewModel() {
+    private val initialEvents = List(10_000, ::createEvent)
+
+    var state by mutableStateOf(
+        EventListState(
+            events = initialEvents,
+            groupedEvents = groupByDate(initialEvents)
+        )
+    )
         private set
     private val _effects = MutableSharedFlow<EventEffect>(extraBufferCapacity = 1)
     val effects = _effects.asSharedFlow()
 
     fun accept(action: EventAction) {
         when (action) {
-            EventAction.Like -> {
-                state = state.copy(
-                    likes = if (state.likedByMe) state.likes - 1 else state.likes + 1,
-                    likedByMe = !state.likedByMe
-                )
-            }
-
-            EventAction.Participate -> {
-                state = state.copy(
-                    participants = if (state.participatedByMe) state.participants - 1 else state.participants + 1,
-                    participatedByMe = !state.participatedByMe
-                )
-            }
-
-            EventAction.Share -> {
-                _effects.tryEmit(EventEffect.ShowToast(R.string.not_implemented))
-            }
-
-            EventAction.Menu -> {
-                _effects.tryEmit(EventEffect.ShowToast(R.string.not_implemented))
-            }
+            is EventAction.Like -> likeById(action.id)
+            is EventAction.Participate -> participateById(action.id)
+            is EventAction.Share -> _effects.tryEmit(EventEffect.ShowToast(R.string.not_implemented))
+            is EventAction.Menu -> _effects.tryEmit(EventEffect.ShowToast(R.string.not_implemented))
         }
     }
 
-    private fun createEvent() = EventUiState(
-        id = 1L,
+    private fun likeById(id: Long) {
+        updateState(state.events.map {
+            if (it.id == id) it.copy(
+                likes = if (it.likedByMe) it.likes - 1 else it.likes + 1,
+                likedByMe = !it.likedByMe
+            ) else it
+        })
+    }
+
+    private fun participateById(id: Long) {
+        updateState(state.events.map {
+            if (it.id == id) it.copy(
+                participants = if (it.participatedByMe) it.participants - 1 else it.participants + 1,
+                participatedByMe = !it.participatedByMe
+            ) else it
+        })
+    }
+
+    private fun updateState(newList: List<EventUiState>) {
+        state = state.copy(
+            events = newList,
+            groupedEvents = groupByDate(newList)
+        )
+    }
+
+    private fun groupByDate(events: List<EventUiState>): Map<Instant, List<EventUiState>> {
+        return events.groupBy {
+            it.published.atZone(ZoneId.systemDefault())
+                .toLocalDate()
+                .atStartOfDay(ZoneId.systemDefault())
+                .toInstant()
+        }
+    }
+
+    private fun createEvent(id: Int) = EventUiState(
+        id = id + 1L,
         author = "Lydia Westervelt",
-        published = "11.05.22 11:21",
+        published = Instant.now().minusSeconds(id / 3 * 24 * 3600L),
         type = EventType.OFFLINE,
-        datetime = "16.05.22 12:00",
-        content = "Приглашаю провести уютный вечер за увлекательными играми! У нас есть несколько вариантов настолок, подходящих для любой компании.",
+        datetime = Instant.now().minusSeconds(id / 3 * 24 * 3600L),
+        content = "$id: Приглашаю провести уютный вечер за увлекательными играми! У нас есть несколько вариантов настолок, подходящих для любой компании.",
         link = "https://m2.material.io/components/cards",
         likes = 2,
         participants = 2
