e9d1572cb80aa253e43772cad5e55c166199bb9a
Aleksandr Iurk <yurk.9999@mail.ru>
2026-05-17 01:41:47 +0700

    Try migrations for room database

--- app/src/main/java/com/eltex/androidschool/data/AppDb.kt
+++ app/src/main/java/com/eltex/androidschool/data/AppDb.kt
@@ -1,17 +1,30 @@
 package com.eltex.androidschool.data
 
 import android.content.Context
+import androidx.room.AutoMigration
 import androidx.room.Database
+import androidx.room.DeleteColumn
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import androidx.room.migration.AutoMigrationSpec
 import com.eltex.androidschool.feauture.event.data.EventEntity
 import com.eltex.androidschool.feauture.event.data.EventsDao
 
-@Database(entities = [EventEntity::class], version = 1)
+@Database(
+    entities = [EventEntity::class],
+    version = 3,
+    autoMigrations = [
+        AutoMigration(from = 1, to = 2),
+        AutoMigration(from = 2, to = 3, spec = AppDb.Migration2to3::class)
+    ]
+)
 @TypeConverters(Converters::class)
 abstract class AppDb : RoomDatabase() {
     abstract val eventsDao: EventsDao
+
+    @DeleteColumn(tableName = "events", columnName = "newProperty")
+    class Migration2to3 : AutoMigrationSpec
 
     companion object {
         @Volatile
